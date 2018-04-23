95b91f0e890526064ddaea991981ddbb5b9b5bfa
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXGrid_CustomColumnChooser/Window1.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using DevExpress.Wpf.Grid;
+
+namespace DXGrid_CustomColumnChooser {
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+            grid.DataSource = IssueList.GetData();
+            InitCustomColumnChooser(tableView);
+            
+        }
+        void InitCustomColumnChooser(GridViewBase view) {
+            columnChooser.View = view;
+            view.ColumnChooserFactory = new CustomColumnChooser(columnChooser);
+            view.ShowColumnChooser();
+        }
+    }
+    public class CustomColumnChooser : IColumnChooser, IColumnChooserFactory {
+        readonly CustomColumnChooserControl columnChooserControl;
+        public CustomColumnChooser(CustomColumnChooserControl columnChooserControl) {
+            this.columnChooserControl = columnChooserControl;
+        }
+        #region IColumnChooser Members
+        void IColumnChooser.Show() { }
+        void IColumnChooser.Hide() { }
+        void IColumnChooser.ApplyState(IColumnChooserState state) { }
+        void IColumnChooser.SaveState(IColumnChooserState state) { }
+        void IColumnChooser.Destroy() { }
+        UIElement IColumnChooser.TopContainer { get {
+                 return columnChooserControl.ColunmChooserControl; } }
+        #endregion
+
+        #region IColumnChooserFactory Members
+        IColumnChooser IColumnChooserFactory.Create(GridViewBase view) {
+            return this;
+        }
+        #endregion
+    }
+    public class CustomColumnChooserControl : Control {
+        public static readonly DependencyProperty ViewProperty;
+
+        static CustomColumnChooserControl() {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomColumnChooserControl),
+                new FrameworkPropertyMetadata(typeof(CustomColumnChooserControl)));
+            ViewProperty = DependencyProperty.Register("View", typeof(GridViewBase),
+                typeof(CustomColumnChooserControl), new UIPropertyMetadata(null));
+        }
+
+        public GridViewBase View {
+            get { return (GridViewBase)GetValue(ViewProperty); }
+            set { SetValue(ViewProperty, value); }
+        }
+
+        internal ColumnChooserControl ColunmChooserControl { get; private set; }
+
+        public override void OnApplyTemplate() {
+            base.OnApplyTemplate();
+            ColunmChooserControl = (ColumnChooserControl)GetTemplateChild(
+                "PART_ColumnChooserControl");
+        }
+    }
+    public class IssueList {
+        static public List<IssueDataObject> GetData() {
+            List<IssueDataObject> data = new List<IssueDataObject>();
+            data.Add(new IssueDataObject() { IssueName = "Transaction History", 
+                IssueType = "Bug", IsPrivate = true });
+            data.Add(new IssueDataObject() { IssueName = "Ledger: Inconsistency",
+                IssueType = "Bug", IsPrivate = false });
+            data.Add(new IssueDataObject() { IssueName = "Data Import",
+                IssueType = "Request", IsPrivate = false });
+            data.Add(new IssueDataObject() { IssueName = "Data Archiving",
+                IssueType = "Request", IsPrivate = true });
+            return data;
+        }
+    }
+
+    public class IssueDataObject {
+        public string IssueName { get; set; }
+        public string IssueType { get; set; }
+        public bool IsPrivate { get; set; }
+    }
+}
---
+++ VB/DXGrid_CustomColumnChooser/Window1.xaml.vb
@@ -0,0 +1,137 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Collections.Generic
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Documents
+Imports DevExpress.Wpf.Grid
+
+Namespace DXGrid_CustomColumnChooser
+	Partial Public Class Window1
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+			grid.DataSource = IssueList.GetData()
+			InitCustomColumnChooser(tableView)
+
+		End Sub
+		Private Sub InitCustomColumnChooser(ByVal view As GridViewBase)
+			columnChooser.View = view
+			view.ColumnChooserFactory = New CustomColumnChooser(columnChooser)
+			view.ShowColumnChooser()
+		End Sub
+	End Class
+	Public Class CustomColumnChooser
+		Implements IColumnChooser, IColumnChooserFactory
+		Private ReadOnly columnChooserControl As CustomColumnChooserControl
+		Public Sub New(ByVal columnChooserControl As CustomColumnChooserControl)
+			Me.columnChooserControl = columnChooserControl
+		End Sub
+		#Region "IColumnChooser Members"
+		Private Sub Show() Implements IColumnChooser.Show
+		End Sub
+		Private Sub Hide() Implements IColumnChooser.Hide
+		End Sub
+		Private Sub ApplyState(ByVal state As IColumnChooserState) _
+				Implements IColumnChooser.ApplyState
+		End Sub
+		Private Sub SaveState(ByVal state As IColumnChooserState) _
+				Implements IColumnChooser.SaveState
+		End Sub
+		Private Sub Destroy() Implements IColumnChooser.Destroy
+		End Sub
+		Private ReadOnly Property TopContainer() As UIElement _
+				Implements IColumnChooser.TopContainer
+			Get
+				Return columnChooserControl.ColunmChooserControl
+			End Get
+		End Property
+		#End Region
+
+		#Region "IColumnChooserFactory Members"
+		Private Function Create(ByVal view As GridViewBase) As IColumnChooser _
+				Implements IColumnChooserFactory.Create
+			Return Me
+		End Function
+		#End Region
+	End Class
+	Public Class CustomColumnChooserControl
+		Inherits Control
+		Public Shared ReadOnly ViewProperty As DependencyProperty
+
+		Shared Sub New()
+			DefaultStyleKeyProperty.OverrideMetadata(GetType(CustomColumnChooserControl), _
+				New FrameworkPropertyMetadata(GetType(CustomColumnChooserControl)))
+			ViewProperty = DependencyProperty.Register("View", GetType(GridViewBase), _
+				GetType(CustomColumnChooserControl), New UIPropertyMetadata(Nothing))
+		End Sub
+
+		Public Property View() As GridViewBase
+			Get
+				Return CType(GetValue(ViewProperty), GridViewBase)
+			End Get
+			Set(ByVal value As GridViewBase)
+				SetValue(ViewProperty, value)
+			End Set
+		End Property
+
+		Private privateColunmChooserControl As ColumnChooserControl
+		Friend Property ColunmChooserControl() As ColumnChooserControl
+			Get
+				Return privateColunmChooserControl
+			End Get
+			Private Set(ByVal value As ColumnChooserControl)
+				privateColunmChooserControl = value
+			End Set
+		End Property
+
+		Public Overrides Sub OnApplyTemplate()
+			MyBase.OnApplyTemplate()
+			ColunmChooserControl = CType(GetTemplateChild("PART_ColumnChooserControl"), _
+				ColumnChooserControl)
+		End Sub
+	End Class
+	Public Class IssueList
+		Public Shared Function GetData() As List(Of IssueDataObject)
+			Dim data As New List(Of IssueDataObject)()
+			data.Add(New IssueDataObject() With {.IssueName = "Transaction History", _
+				.IssueType = "Bug", .IsPrivate = True})
+			data.Add(New IssueDataObject() With {.IssueName = "Ledger: Inconsistency", _
+				.IssueType = "Bug", .IsPrivate = False})
+			data.Add(New IssueDataObject() With {.IssueName = "Data Import", _
+				.IssueType = "Request", .IsPrivate = False})
+			data.Add(New IssueDataObject() With {.IssueName = "Data Archiving", _
+				.IssueType = "Request", .IsPrivate = True})
+			Return data
+		End Function
+	End Class
+
+	Public Class IssueDataObject
+		Private privateIssueName As String
+		Public Property IssueName() As String
+			Get
+				Return privateIssueName
+			End Get
+			Set(ByVal value As String)
+				privateIssueName = value
+			End Set
+		End Property
+		Private privateIssueType As String
+		Public Property IssueType() As String
+			Get
+				Return privateIssueType
+			End Get
+			Set(ByVal value As String)
+				privateIssueType = value
+			End Set
+		End Property
+		Private privateIsPrivate As Boolean
+		Public Property IsPrivate() As Boolean
+			Get
+				Return privateIsPrivate
+			End Get
+			Set(ByVal value As Boolean)
+				privateIsPrivate = value
+			End Set
+		End Property
+	End Class
+End Namespace
